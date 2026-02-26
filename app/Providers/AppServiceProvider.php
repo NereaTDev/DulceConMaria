ec0d7682cfc3da3c307145850f210b1cfb22a8ba
Ash3-23 <nerea.trebol@yahoo.es>
2026-02-26 19:20:43 +0100

    Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar HTTPS en producción para evitar contenido mixto (formularios y assets)
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
